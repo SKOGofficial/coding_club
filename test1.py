9101b5b58f84dd61864502d48b4014d98c8f332a
skogofficial <skogofficial@gmail.com>
2023-10-19 22:20:46 -0400

    official website with link to previos website

--- test1.py
+++ test1.py
@@ -4,3 +4,4 @@ print(":D - from Daphne")
 print(">:D - from Lynn (but not really)")
 print(":D")
 print("check 2 ")
+# Im thinking of doing some flask in the backend: don't really know what kind of backend we will want for this rpoject tho
